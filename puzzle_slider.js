286afef94b91934c3f57d4f7393bdf3cf909879d
Kaszydo <kaszydo@gmail.com>
2023-05-30 18:12:22 +0200

    Refactoring

--- puzzle_slider.js
+++ puzzle_slider.js
@@ -6,7 +6,7 @@ const gameState = [
   [puzzles[6], puzzles[7], puzzles[8]],
 ];
 
-// return clicked puzzle position in gameState
+// Return clicked puzzle position in gameState
 function getPuzzlePositionOnBoard(target) {
   let x, y;
   gameState.forEach((row, rowIndex) => {
@@ -20,7 +20,7 @@ function getPuzzlePositionOnBoard(target) {
   return [x, y];
 }
 
-// return empty puzzle in gameState
+// Return empty puzzle position in gameState
 function getEmptyPosition() {
   let emptyX, emptyY;
   gameState.forEach((row, rowIndex) => {
@@ -34,60 +34,32 @@ function getEmptyPosition() {
   return [emptyX, emptyY];
 }
 
-// check if puzzle can move
+// Check if puzzle can move
 function checkIfValidMove(x, y, emptyX, emptyY) {
   return (
     (emptyX === x + 1 && emptyY === y) || // down
-    (emptyX === x - 1 && emptyY === y) || //  up
+    (emptyX === x - 1 && emptyY === y) || // up
     (emptyX === x && emptyY === y + 1) || // right
     (emptyX === x && emptyY === y - 1) // left
   );
 }
 
-//switch puzzle with empty space
-function movePuzzle(event) {
-  let [x, y] = getPuzzlePositionOnBoard(event.target);
-  let [emptyX, emptyY] = getEmptyPosition();
-  if (checkIfValidMove(x, y, emptyX, emptyY)) {
-    let puzzleHTML = gameState[x][y].innerHTML;
-    let emptyPuzzle = gameState[emptyX][emptyY];
-    emptyPuzzle.innerHTML += puzzleHTML;
-    while (gameState[x][y].firstChild) {
-      gameState[x][y].removeChild(gameState[x][y].firstChild);
-    }
-    console.log("Puzzle moved");
-  }
+// Move puzzle to the empty space
+function movePuzzle(x, y, emptyX, emptyY) {
+  let puzzleHTML = gameState[x][y].innerHTML;
+  let emptyPuzzle = gameState[emptyX][emptyY];
+  emptyPuzzle.innerHTML += puzzleHTML;
+  gameState[x][y].innerHTML = "";
+  console.log("Puzzle moved");
 }
 
-// listiners for checking if functions work
+// Puzzle click event listener
 function puzzleClickListener(event) {
-  console.log("clicked");
-}
-
-function puzzlePositionListener(event) {
   let [x, y] = getPuzzlePositionOnBoard(event.target);
-  console.log(`Puzzle; Row: ${x}, Column: ${y}`);
+  console.log(`Clicked: Row: ${x}, Column: ${y}`);
 }
 
-function emptyPositionListener(event) {
-  let [emptyX, emptyY] = getEmptyPosition(event.target);
-  console.log(`Empty was: Row: ${emptyX}, Column: ${emptyY}`);
-}
-
-function validMoveListener(event) {
-  let [x, y] = getPuzzlePositionOnBoard(event.target);
-  let [emptyX, emptyY] = getEmptyPosition();
-  if (checkIfValidMove(x, y, emptyX, emptyY)) {
-    console.log("true");
-  } else {
-    console.log("false");
-  }
-}
-
-function puzzleMoveListener(event) {
-  movePuzzle(event);
-}
-
+// Puzzle number click event listener
 function puzzleNumberListener(event) {
   let puzzle = event.target;
   if (puzzle.tagName === "IMG") {
@@ -97,12 +69,17 @@ function puzzleNumberListener(event) {
   }
 }
 
+// Add event listeners to puzzles
 puzzles.forEach((puzzle) => {
   puzzle.addEventListener("click", puzzleClickListener);
-  puzzle.addEventListener("click", puzzlePositionListener);
-  puzzle.addEventListener("click", emptyPositionListener);
-  puzzle.addEventListener("click", validMoveListener);
-  puzzle.addEventListener("click", movePuzzle);
-  puzzle.addEventListener("click", puzzleMoveListener);
   puzzle.addEventListener("click", puzzleNumberListener);
+});
+
+// Puzzle move event listener
+board.addEventListener("click", function (event) {
+  let [x, y] = getPuzzlePositionOnBoard(event.target);
+  let [emptyX, emptyY] = getEmptyPosition();
+  if (checkIfValidMove(x, y, emptyX, emptyY)) {
+    movePuzzle(x, y, emptyX, emptyY);
+  }
 });
